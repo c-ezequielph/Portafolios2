bad146d4982a1c1cbe99e52216009b71040f0804
c-ezequielph <55009047+c-ezequielph@users.noreply.github.com>
2019-10-31 19:56:18 -0400

    Add files via upload

---
+++ Punto1P2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    char parr[1000], *punt;
+    printf("Digite su frase");
+    gets(parr);
+
+    return 0;
+}
+
+char * ejerc (char *parrafo){
+    while (*parrafo!="/0"){
+        if (*parrafo == '.' || *parrafo == '!' || *parrafo =='?')
+
+    }
+
+
+}
